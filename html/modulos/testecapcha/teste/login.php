8fcd0748a9c163755212c15a21fbba1d8b5292db
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-08-17 15:46:11 -0300

    feito o CAPCHA no site do Coopex

--- html/modulos/testecapcha/teste/login.php
+++ html/modulos/testecapcha/teste/login.php
@@ -1,4 +1,38 @@
 <?php
+const SECRET_KEY = '0x4AAAAAAAIvyg8pyRfleu82akF5MdSCvto';
 
-    echo "ola mundo";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $token = $_POST['cf-turnstile-response']; // Captcha response token
+    $ip = $_SERVER['HTTP_CF_CONNECTING_IP']; 
+
+    $formData = array(
+        'secret' => SECRET_KEY,
+        'response' => $token,
+        'remoteip' => $ip
+    );
+
+    $url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-Type: application/x-www-form-urlencoded',
+            'content' => http_build_query($formData),
+            'ignore_errors' => true // Allow reading error responses
+        )
+    );
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === false) {
+        echo "Error while verifying CAPTCHA.";
+    } else {
+        $outcome = json_decode($result, true);
+        if ($outcome['success']) {
+            echo "Verification succeeded!";
+        } else {
+            echo "Verification failed.";
+        }
+    }
+}
 ?>
